808e2dbe985dc219bc3a2a9857ee94d0ed5dd647
anandg112 <anandg112@gmail.com>
2018-08-18 00:59:15 -0400

    add Procfile and bind port to server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", function () {
-    console.log("Listening on Port 3000");
-});
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/'));
+app.listen(process.env.PORT || 8080);
